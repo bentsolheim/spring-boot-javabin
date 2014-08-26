1a8f7fec17abb6ffec5bb7c21b841a145bbc5865
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2014-08-26 23:21:23 +0200

    Added spring-boot support

--- src/main/groovy/no/javabin/springboot/Main.groovy
+++ src/main/groovy/no/javabin/springboot/Main.groovy
@@ -1,8 +1,13 @@
 package no.javabin.springboot
 
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+
+@EnableAutoConfiguration
 class Main {
 
     public static void main(String[] args) {
 
+        SpringApplication.run(Main, args)
     }
 }
