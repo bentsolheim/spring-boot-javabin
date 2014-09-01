6cbbda764e22c5487239520c1593b471a9077e0f
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2014-09-01 22:13:40 +0200

    Initializing database with a couple of sample users

--- src/main/groovy/no/javabin/springboot/Main.groovy
+++ src/main/groovy/no/javabin/springboot/Main.groovy
@@ -1,7 +1,10 @@
 package no.javabin.springboot
 
+import no.javabin.springboot.domain.User
+import no.javabin.springboot.repos.UserRepository
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
@@ -12,6 +15,10 @@ class Main {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(Main, args)
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Main, args)
+        UserRepository userRepository = applicationContext.getBean(UserRepository)
+
+        userRepository.save(new User(firstName: 'Bent', lastName: 'Solheim', email: 'bent@kodehuset.no'))
+        userRepository.save(new User(firstName: 'Jaran', lastName: 'Nilsen', email: 'jaran@kodehuset.no'))
     }
 }
